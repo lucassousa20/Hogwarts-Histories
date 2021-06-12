67df7fb4672c19e9a1d6a8de00037bb130f075d2
lucassousa20 <60761607+lucassousa20@users.noreply.github.com>
2021-06-12 18:46:08 -0300

    cards

--- app/src/main/java/br/com/ddm/hogwartshistories/DetalheDisciplinaActivity.kt
+++ app/src/main/java/br/com/ddm/hogwartshistories/DetalheDisciplinaActivity.kt
@@ -2,10 +2,15 @@ package br.com.ddm.hogwartshistories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class DetalheDisciplinaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhe_disciplina)
+
+        val disciplina =  intent.extras?.getSerializable("disciplina") as Disciplina
+
+        Toast.makeText(this, "${disciplina.nome}", Toast.LENGTH_SHORT).show()
     }
 }
